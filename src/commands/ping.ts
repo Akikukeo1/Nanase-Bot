c1a1c9c611bd3aaedd3817be3b6fa25da55a93fa
Akikukeo1 (Keisuke Ishii) <keisuke.ishii.01@gmail.com>
2026-09-03 18:36:37 +0900

    style: format project files

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -3,17 +3,17 @@ import { Command } from "../types/command";
 import botConfig from "../../bot.config";
 
 export default {
-    data: {
-        name: "ping",
-        flags: MessageFlags.Ephemeral,
-        description: "Ping command"
-    },
-    async execute(interaction: CommandInteraction): Promise<void> {
-        const embed = new EmbedBuilder()
-            .setTitle("Pong!")
-            .setDescription(`WebSocket Ping: ${interaction.client.ws.ping}ms`)
-            .setColor(0x00FF00);
-        await interaction.followUp({ embeds: [embed], allowedMentions: { roles: [botConfig.role.moderatorId] } });
-        return;
-    }
+	data: {
+		name: "ping",
+		flags: MessageFlags.Ephemeral,
+		description: "Ping command",
+	},
+	async execute(interaction: CommandInteraction): Promise<void> {
+		const embed = new EmbedBuilder()
+			.setTitle("Pong!")
+			.setDescription(`WebSocket Ping: ${interaction.client.ws.ping}ms`)
+			.setColor(0x00ff00);
+		await interaction.followUp({ embeds: [embed], allowedMentions: { roles: [botConfig.role.moderatorId] } });
+		return;
+	},
 } as Command;
